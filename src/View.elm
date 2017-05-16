fcecdb5b4428e5a015b8e498ef6a3dc66a2dd8ea
abab <ababup1192@gmail.com>
2017-05-16 23:00:44 +0900

    Add VisualEditor

--- src/View.elm
+++ src/View.elm
@@ -2,23 +2,43 @@ module View exposing (view)
 
 import Html exposing (..)
 import Html.Attributes as Attr
-import Html.Events exposing (onInput)
+import Svg exposing (..)
+import Svg.Attributes as SvgAttr
 
 import Messages exposing (..)
 import Models exposing (Model)
 
-
 -- View
 view : Model -> Html Msg
 view ( { code } as model) =
-    div 
-        [ Attr.id "editor"
+    div [ Attr.class "hybridEditor"
+        ] 
+     [
+        textEditor,
+        visualEditor
+    ]
+
+textEditor : Html Msg
+textEditor = 
+    div [ Attr.id "editor"
         , Attr.style [ ("position", "relative")
-          , ("width",  pixels 700)
-          , ("height", pixels 500)
+            , ("width",  pixels 450)
+            , ("height", pixels 500)
           ]
         ] 
     [ ]
+
+visualEditor : Html Msg
+visualEditor = 
+    svg [ SvgAttr.viewBox "0 0 100 100"
+        , SvgAttr.width <| pixels 350
+        , SvgAttr.class "visualEditor"
+        ]
+        [ circle [ SvgAttr.cx "50", SvgAttr.cy "50", 
+            SvgAttr.r "3", SvgAttr.fill "#0B79CE" ] []
+        ]
+
+
 
 pixels : Int -> String
 pixels n = toString n ++ "px"
