e2ba44ae5a46302d1aa3b65a682a7f5562cc5b1c
abab <ababup1192@gmail.com>
2017-05-24 17:59:35 +0900

    Add case of updateCode Test

--- tests/UpdateTest.elm
+++ tests/UpdateTest.elm
@@ -4,6 +4,7 @@ import Test exposing (..)
 import TestExp exposing (..)
 import Update exposing (..)
 import DotsParser.Ast as Ast
+import Models exposing (..)
 
 
 all : Test
@@ -20,4 +21,17 @@ updateCodeTest =
             => Tuple.first
                 (updateCode { code = "", ast = Ast.rootNode [], drag = Nothing } { code = "[]" })
             === { code = "[]", ast = Ast.rootNode [ Ast.listNode [ 0 ] [] ], drag = Nothing }
+        , "updateCode [(1, 2), (3, 4)]"
+            => Tuple.first
+                (updateCode { code = "[]", ast = Ast.rootNode [], drag = Nothing } { code = "[(1, 2), (3, 4)]" })
+            === { code = "[(1, 2), (3, 4)]"
+                , ast =
+                    Ast.rootNode
+                        [ Ast.listNode [ 0 ]
+                            [ Ast.positionNode [ 0, 0 ] [] { x = 1, y = 2 }
+                            , Ast.positionNode [ 0, 1 ] [] { x = 3, y = 4 }
+                            ]
+                        ]
+                , drag = Nothing
+                }
         ]
