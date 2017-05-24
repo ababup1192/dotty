65300077c98980c8c792389ff0079505919643ed
abab <ababup1192@gmail.com>
2017-05-24 20:02:24 +0900

    Add update drag test

--- tests/UpdateTest.elm
+++ tests/UpdateTest.elm
@@ -4,6 +4,7 @@ import Test exposing (..)
 import TestExp exposing (..)
 import Update exposing (..)
 import DotsParser.Ast as Ast
+import Models exposing (Drag)
 
 
 all : Test
@@ -32,5 +33,30 @@ updateCodeTest =
                             ]
                         ]
                 , drag = Nothing
+                }
+        , "updateCode [(1, 2), (102, 103)] with Drag"
+            => Tuple.first
+                (updateCode
+                    { code = "[(1, 2), (102, 103)]"
+                    , ast =
+                        Ast.rootNode
+                            [ Ast.listNode [ 0 ]
+                                [ Ast.positionNode [ 0, 0 ] [] { x = 1, y = 2 }
+                                , Ast.positionNode [ 0, 1 ] [] { x = 3, y = 4 }
+                                ]
+                            ]
+                    , drag = Just <| Drag { x = 3, y = 4 } { x = 102, y = 103 } [ 0, 1 ]
+                    }
+                    { code = "[(1, 2), (102, 103)]" }
+                )
+            === { code = "[(1, 2), (102, 103)]"
+                , ast =
+                    Ast.rootNode
+                        [ Ast.listNode [ 0 ]
+                            [ Ast.positionNode [ 0, 0 ] [] { x = 1, y = 2 }
+                            , Ast.positionNode [ 0, 1 ] [] { x = 3, y = 4 }
+                            ]
+                        ]
+                , drag = Just <| Drag { x = 3, y = 4 } { x = 102, y = 103 } [ 0, 1 ]
                 }
         ]
