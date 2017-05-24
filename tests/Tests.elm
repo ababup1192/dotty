469ab8d5245176359f969bd92c395d6604de2022
abab <ababup1192@gmail.com>
2017-05-24 15:09:23 +0900

    Add Unparse with drag test

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -7,17 +7,19 @@ import TestExp exposing (..)
 import DotsParser.Ast exposing (..)
 import DotsParser.Parser exposing (dotsParser, parse)
 import DotsParser.Unparser exposing (unparse)
+import Models exposing (Drag)
 
 
 all : Test
 all =
     describe "PointsParser module Test" <|
         [ parserTest
-        , updatePositionTest
-        , unparserTest
         , getPositionTest
         , insertPositionTest
+        , updatePositionTest
         , ast2PositionsTest
+        , unparserTest
+        , unparserWithDragTest
         ]
 
 
@@ -92,39 +94,22 @@ unparserTest =
             => unparse (rootNode [ listNode [ 0 ] [ positionNode [ 0, 0 ] [] { x = 1, y = 2 } ] ]) Nothing
             === (Ok "[(1, 2)]")
         , "unparseAst Root <| NList [NPoint 1 2, 3 4]"
-            => unparse (rootNode [ listNode [ 0 ] [ positionNode [ 0, 0 ] [] { x = 1, y = 2 }, positionNode [ 0, 0 ] [] { x = 3, y = 4 } ] ]) Nothing
+            => unparse (rootNode [ listNode [ 0 ] [ positionNode [ 0, 0 ] [] { x = 1, y = 2 }, positionNode [ 0, 1 ] [] { x = 3, y = 4 } ] ]) Nothing
             === (Ok "[(1, 2), (3, 4)]")
         ]
 
 
-
---  , "result equals between [(1,2),(3,4)] and [ ( 1 , 2 ) , ( 3 , 4 ) ]" =>
---      parse "[(1,2),(3,4)]" === parse "[ ( 1 , 2 ) , ( 3 , 4 ) ]"
---
---  , "unparseAst Root <| NList [NDot 1 2]" =>
---      unparseAst (Root <| NList [NPoint 1 2]) === (Ok "[(1, 2)]")
---
---  , "unparseAst Root <| NList [NDot 1 2, NDot 3 4]" =>
---      unparseAst (Root <| NList [NPoint 1 2, NPoint 3 4]) === (Ok "[(1, 2), (3, 4)]")
---
---  , "toAst []" =>
---      toAst [] === (Root <| NList [])
---
---  , "toAst [(1, 2)]" =>
---      toAst [(1, 2)] === (Root <| NList [NPoint 1 2])
---
---  , "toAst [(1, 2), (3, 4)]" =>
---      toAst [(1, 2), (3, 4)] === (Root <| NList [NPoint 1 2, NPoint 3 4])
---
---  , "toAst [(1, 2), (3, 4)]" =>
---      toAst [(1, 2), (3, 4)] === (Root <| NList [NPoint 1 2, NPoint 3 4])
---
---  , "unparse []" =>
---      unparse [] === Ok "[]"
---
---  , "unparse [(1, 2)]" =>
---      unparse [(1, 2)] === Ok "[(1, 2)]"
---
---  , "unparse [(1, 2), (3, 4)]" =>
---      unparse [(1, 2), (3, 4)] === Ok "[(1, 2), (3, 4)]"
---  ]
+unparserWithDragTest : Test
+unparserWithDragTest =
+    describe "DotsParser.Unparser Test" <|
+        [ "unparseAst Root <| NList [] with Just Drag"
+            => unparse
+                (rootNode [ listNode [ 0 ] [] ])
+                (Just <| Drag { x = 0, y = 0 } { x = 0, y = 0 } [ 0, 0 ])
+            === (Ok "[]")
+        , "unparseAst Root <| NList [NPoint 1 2, 3 4] with Just Drag"
+            => unparse
+                (rootNode [ listNode [ 0 ] [ positionNode [ 0, 0 ] [] { x = 1, y = 2 }, positionNode [ 0, 1 ] [] { x = 3, y = 4 } ] ])
+                (Just <| Drag { x = 3, y = 4 } { x = 10, y = 10 } [ 0, 1 ])
+            === (Ok "[(1, 2), (10, 10)]")
+        ]
